b83b02ad5813947457d68102b1c4e90732ddbd0d
yoshida-1992 <58692808+yoshida-1992@users.noreply.github.com>
2023-09-03 23:10:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const EXECUTE_URL = process.env.EXECUTE_URL
 const FORM_URL = process.env.FORM_URL
 
 exports.handler =  function(event, context, callback) { 
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  console.log("success!!")
 }
